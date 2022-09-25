7f310d07968ab75dbe57cccf517e21ae235f7f26
Tarek Tabet <t.tabet@outlook.com>
2022-09-25 16:19:08 +1000

    Initial Commit

---
+++ Source/TheWise/TheWiseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheWiseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEWISE_API ATheWiseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
