198ca5a807cd91ae7cfafc75fa43671f8e13b519
Tarek Tabet <t.tabet@outlook.com>
2022-09-27 19:40:07 +1000

    Remaking Character Controller
    - Set Up Animation and Synty Character
    - Movement Started
    
    - To Do: Complete Movement and Camera Movement

--- Source/TheWise/Private/TTCharacter.cpp
+++ Source/TheWise/Private/TTCharacter.cpp
@@ -3,13 +3,29 @@
 
 #include "TTCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 // Sets default values
 ATTCharacter::ATTCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+	
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->SetupAttachment(SpringArmComp);
+
 }
+
+void ATTCharacter::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	
+}
+
 
 // Called when the game starts or when spawned
 void ATTCharacter::BeginPlay()
@@ -30,5 +46,13 @@ void ATTCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("Move Forward / Backward", this, &ATTCharacter::MoveForward);
+	
+	PlayerInputComponent->BindAxis("Turn Right / Left Mouse", this, &APawn::AddControllerYawInput);
+	
 }
 
+void ATTCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
--- Source/TheWise/Public/TTCharacter.h
+++ Source/TheWise/Public/TTCharacter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "TTCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class THEWISE_API ATTCharacter : public ACharacter
 {
@@ -16,13 +19,25 @@ public:
 	ATTCharacter();
 
 protected:
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+
+	// Called after the actor's components have been initialized, but before BeginPlay()
+    virtual void PostInitializeComponents() override;
+	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	void MoveForward(float Value);
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
